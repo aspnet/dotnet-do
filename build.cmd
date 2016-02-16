0e216d682e55f3858c2b937d58727a552e9230a5
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-15 16:53:28 -0800

    add dotnet-cli bootstrapping to build script

--- build.cmd
+++ build.cmd
@@ -1,26 +1,2 @@
 @echo off
-
-setlocal
-
-cd %~dp0
-
-echo Restoring packages for build tasks
-dotnet restore
-if errorlevel 1 goto fail
-
-echo Building build tasks
-del /s /q "%~dp0tasks\bin\app"
-dotnet build --framework dnxcore50 "%~dp0tasks" -o "%~dp0tasks\bin\app"
-if errorlevel 1 goto fail
-
-echo Running build tasks
-%~dp0\tasks\bin\app\tasks.exe %*
-if errorlevel 1 goto fail
-
-echo Build completed
-goto end
-
-:fail
-echo Build failed
-
-:end
+powershell -NoProfile -NoLogo -File %~dp0\build.ps1 %*
---
+++ build.ps1
@@ -0,0 +1,22 @@
+cd $PSScriptRoot
+
+if(!(Get-Command dotnet -ErrorAction SilentlyContinue))
+{
+	Write-Host "Installing dotnet-cli"
+	iwr https://github.com/dotnet/cli/blob/rel/1.0.0/scripts/obtain/install.ps1 | iex
+}
+
+Write-Host "Restoring packages for build tasks"
+dotnet restore
+if($LASTEXITCODE -ne 0) { throw "Build failed" }
+
+Write-Host "Building build tasks"
+del -rec -for "$PSScriptRoot\tasks\bin\app"
+dotnet build --framework dnxcore50 "$PSScriptRoot\tasks" -o "$PSScriptRoot\tasks\bin\app"
+if($LASTEXITCODE -ne 0) { throw "Build failed" }
+
+Write-Host "Running build tasks"
+& "$PSScriptRoot\tasks\bin\app\tasks.exe" @args
+if($LASTEXITCODE -ne 0) { throw "Build failed" }
+
+Write-Host "Build completed"
