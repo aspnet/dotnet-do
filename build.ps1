2e5168943133c5f12079a2a2b31632bcbed74a82
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-18 11:04:40 -0800

    more build script bits

--- build.ps1
+++ build.ps1
@@ -4,19 +4,21 @@ $ErrorActionPreference="Stop"
 
 Write-Host "Installing dotnet-cli"
 $dotnetInstall = "$PSScriptRoot\dotnet_install.ps1"
+
+$env:DOTNET_INSTALL_DIR="$PSScriptRoot\.dotnet"
 iwr https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1 -OutFile $dotnetInstall
 & $dotnetInstall -Channel beta
 
-$dotnetPath = (Get-Command dotnet).Definition
-Write-Host "Using CLI from: $dotnetPath"
+$dotnet = Join-Path $env:DOTNET_INSTALL_DIR "bin\dotnet.exe"
+Write-Host "Using CLI from: $dotnet"
 
 Write-Host "Restoring packages for build tasks"
-dotnet restore
+& "$dotnet" restore
 if($LASTEXITCODE -ne 0) { throw "Build failed" }
 
 Write-Host "Building build tasks"
 del -rec -for "$PSScriptRoot\tasks\bin\app"
-dotnet build --framework dnxcore50 "$PSScriptRoot\tasks" -o "$PSScriptRoot\tasks\bin\app"
+& "$dotnet" build --framework dnxcore50 "$PSScriptRoot\tasks" -o "$PSScriptRoot\tasks\bin\app"
 if($LASTEXITCODE -ne 0) { throw "Build failed" }
 
 Write-Host "Running build tasks"
--- build.sh
+++ build.sh
@@ -11,14 +11,17 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 set -e
 
 echo "Installing dotnet-cli"
+export DOTNET_INSTALL_DIR="$DIR/.dotnet"
 curl -s https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.sh | sh -s -- --channel beta
 
+DOTNET="$DOTNET_INSTALL_DIR/share/dotnet/cli/bin/dotnet"
+
 echo "Restoring packages for build tasks"
-dotnet restore
+$DOTNET restore
 
 echo "Building build tasks"
 rm -Rf "$DIR/tasks/bin/app"
-dotnet build --framework dnxcore50 "$DIR/tasks" -o "$DIR/tasks/bin/app"
+$DOTNET build --framework dnxcore50 "$DIR/tasks" -o "$DIR/tasks/bin/app"
 
 echo "Running build tasks"
 $DIR/tasks/bin/app/tasks "$@"
