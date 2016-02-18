443824a71c92ec7f8f9227656c5205427364b45e
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-17 21:12:19 -0800

    build script touch ups

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 set -e
 
 echo "Installing dotnet-cli"
-curl https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.sh | sh -s -- --channel beta
+curl -s https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.sh | sh -s -- --channel beta
 
 echo "Restoring packages for build tasks"
 dotnet restore
@@ -21,6 +21,6 @@ rm -Rf "$DIR/tasks/bin/app"
 dotnet build --framework dnxcore50 "$DIR/tasks" -o "$DIR/tasks/bin/app"
 
 echo "Running build tasks"
-$DIR/tasks/bin/app/tasks "$*"
+$DIR/tasks/bin/app/tasks "$@"
 
 echo "Build completed"
