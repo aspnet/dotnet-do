56af9f32c53b2eb70f7bfdaf7d2f55951bc1fa2c
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-18 12:15:31 -0800

    remove verbosity

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ curl -s https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/in
 DOTNET="$DOTNET_INSTALL_DIR/share/dotnet/cli/bin/dotnet"
 
 echo "Restoring packages for build tasks"
-$DOTNET restore --verbosity Verbose
+$DOTNET restore
 
 echo "Building build tasks"
 rm -Rf "$DIR/tasks/bin/app"
